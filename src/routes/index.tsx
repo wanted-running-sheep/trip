ac765b0be9c8530ca3992a158c263655f6e001a2
CHOI <tlacjd665@gmail.com>
2022-08-04 14:30:45 +0900

    ♻️ refactor: #19 skeleton 라우터 삭제

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-
 import Layout from '@/components/Layout';
 import { ConfirmPage, MainPage } from '@/pages';
 import { NavigateEnum } from '@/types/enum';
-import Skeleton from '@/components/Skeleton';
 
 const Router = () => {
   return (
@@ -13,7 +11,6 @@ const Router = () => {
         <Route path={NavigateEnum.MAIN} element={<MainPage />}></Route>
         <Route path={NavigateEnum.CONFIRM} element={<ConfirmPage />} />
       </Route>
-      <Route path="/skeleton" element={<Skeleton />} />
       <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
   );
